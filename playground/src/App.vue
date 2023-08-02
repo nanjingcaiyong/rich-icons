d310c92a523dffc798eccb7edfb139b461e08e57
caiyong <yong.cai@kapeixi.com>
2023-08-02 19:08:56 +0800

    feat: first commit

---
+++ playground/src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { icons } from '@rich-icons/icons-vue/global'
+</script>
+
+<template>
+  <component :is="Icon" v-for="(Icon, key) in icons" :key="key" class="icon" />
+  <hr />
+  <component :is="`RIcon${key}`" v-for="key in Object.keys(icons)" :key="key" class="icon" />
+</template>
+
+<style>
+.icon {
+  height: 48px;
+  color: #409eff;
+}
+</style>
---
+++ playground/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import RIcons from '@rich-icons/icons-vue/global'
+import App from './App.vue'
+
+createApp(App).use(RIcons).mount('#app')
